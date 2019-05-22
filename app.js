1f2639b16d9984882fd1ff502360350a3a1fa089
benedict iyekekpolo <benedictuyioghosa@gmail.com>
2019-05-22 14:29:59 +0100

    make minor changes

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var express = require("express"),
     app = express(),
-    bodyParser = require("body-parser"),
-    xoauth2 = require("xoauth2"),
-    Swal = require('sweetalert2'),
-    nodemailer = require("nodemailer");
+    bodyParser = require("body-parser");
 
-var htmlToText = require('nodemailer-html-to-text').htmlToText;
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -16,57 +12,8 @@ app.get("/", (req, res) => {
 });
 
 
-app.get("/love", (req, res) => {
+app.get("/works", (req, res) => {
     res.render("love");
-});
-
-app.get("/form", (req, res) => { 
-    //var message = "successful";
-    res.render("contact");
-});
-
-app.post("/send", (req, res) => {
-    const output = `
-    <p>You have a new contact request</p>
-    <h3>Contact Details</h3>
-    <ul>
-        <li>Name: ${req.body.name}</li>
-        <li>Company: ${req.body.company}</li>
-        <li>Email: ${req.body.email}</li>
-        <li>Phone: ${req.body.phone}</li>
-    </ul>
-    <h3>Message</h3>
-    <p>${req.body.message}</P>
-    `;
-
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-                type: "OAuth2",
-                user: "benedictuyioghosa@gmail.com",
-                clientId: "88410084868-nsnvcot7clr3c4sup5ob788uh111fv1o.apps.googleusercontent.com",
-                clientSecret: "f3a4wCe_iT-kSBNGUPCMUBtw",
-                refreshToken: "1/M7Ot02cMeiLqbIV_RxCommtViL2b1jdMg1f1zj3JK9M",
-                accessToken: "ya29.GlvpBu7eY24gvywD8OFrX3duoYwZK9N3jXXsPeLZ-cVOQkJN6GKV5LxPOPV0m5darcBosM3NKNq0-3FUTH6IgsExc-mLBk0xonSiMxN86apgUPPxTYJU7uTmmJT8"
-        }
-    });
-    transporter.use('compile', htmlToText());
-    
-    var mailOptions = {
-        from: "Uyi <benedictuyioghosa@gmail.com>",
-        to: "benedictuyioghosa@yahoo.com",
-        subject: "Nodemailer",
-        text: output
-    };
-    
-    transporter.sendMail(mailOptions, (err, res) => {
-        if (err) {
-            console.log(err);
-        } else{
-            console.log("Email sent");
-        }
-    });
-    res.render("contact", {message });
 });
 
 
